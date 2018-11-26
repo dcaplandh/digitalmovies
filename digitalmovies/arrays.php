b1fb66a93f4d60c1b772ace2e459340f894dc177
unknown <Digital@DHC-A2-00.DH.digitalhouse.com>
2018-11-26 10:33:47 -0300

    images y arrays

---
+++ digitalmovies/arrays.php
@@ -0,0 +1,56 @@
+$recientes = [
+    ['title' => 'Race cars', 'year' => 2015, 'image' => 'images/racecar.jpg'],
+    ['title' => 'Dancing', 'year' => 2013, 'image' => 'images/dance.jpg'],
+    ['title' => 'Beach Love', 'year' => 2010, 'image' => 'images/beachlove.jpg'],
+    ['title' => 'Japanese Kitchen', 'year' => 2017, 'image' => 'images/japankitchen.jpg'],
+    ['title' => 'Sport Movie', 'year' => 2015, 'image' => 'images/baseball.jpg'],
+    ['title' => 'Dance Competition', 'year' => 2008, 'image' => 'images/dance2.jpg'],
+    ['title' => 'The Voice', 'year' => 2018, 'image' => 'images/voice.jpg'],
+    ['title' => 'The Voice Revenge', 'year' => 2015, 'image' => 'images/voice2.jpg'],
+    ['title' => 'Sherlock Holmes', 'year' => 2016, 'image' => 'images/holmes.jpg'],
+    ['title' => 'The Voice 2', 'year' => 2012, 'image' => 'images/voice3.jpg'],
+    ['title' => 'Love Stories', 'year' => 2011, 'image' => 'images/love.jpg'],
+    ['title' => 'American Kitchen', 'year' => 2014, 'image' => 'images/kitchen.jpg'],
+    ['title' => 'Modeling, 'year' => 2018, 'image' => 'images/model.jpg'],
+    ['title' => 'Rickys Life', 'year' => 2017, 'image' => 'images/ricky.jpg'],
+    ['title' => 'Marry me', 'year' => 2015, 'image' => 'images/marry.jpg'],
+    ['title' => 'Property Brothers', 'year' => 2013, 'image' => 'images/property.jpg'],
+    ['title' => 'The Rain', 'year' => 2010, 'image' => 'images/rain.jpg']
+];
+
+$sugeridas = [
+    ['title' => 'The Voice 2', 'year' => 2012, 'image' => 'images/voice3.jpg'],
+    ['title' => 'Love Stories', 'year' => 2011, 'image' => 'images/love.jpg'],
+    ['title' => 'American Kitchen', 'year' => 2014, 'image' => 'images/kitchen.jpg'],
+    ['title' => 'Modeling, 'year' => 2018, 'image' => 'images/model.jpg'],
+    ['title' => 'Rickys Life', 'year' => 2017, 'image' => 'images/ricky.jpg'],
+    ['title' => 'Marry me', 'year' => 2015, 'image' => 'images/marry.jpg'],
+    ['title' => 'Property Brothers', 'year' => 2013, 'image' => 'images/property.jpg'],
+    ['title' => 'Race cars', 'year' => 2015, 'image' => 'images/racecar.jpg'],
+    ['title' => 'Dancing', 'year' => 2013, 'image' => 'images/dance.jpg'],
+    ['title' => 'Beach Love', 'year' => 2010, 'image' => 'images/beachlove.jpg'],
+    ['title' => 'Japanese Kitchen', 'year' => 2017, 'image' => 'images/japankitchen.jpg'],
+    ['title' => 'Sport Movie', 'year' => 2015, 'image' => 'images/baseball.jpg'],
+    ['title' => 'Dance Competition', 'year' => 2008, 'image' => 'images/dance2.jpg'],
+    ['title' => 'The Voice', 'year' => 2018, 'image' => 'images/voice.jpg'],
+    ['title' => 'The Voice Revenge', 'year' => 2015, 'image' => 'images/voice2.jpg'],
+    ['title' => 'Sherlock Holmes', 'year' => 2016, 'image' => 'images/holmes.jpg'],
+    ['title' => 'The Rain', 'year' => 2010, 'image' => 'images/rain.jpg']
+];
+
+$masvistas = [
+    ['title' => 'Marry me', 'year' => 2015, 'image' => 'images/marry.jpg'],
+    ['title' => 'The Rain', 'year' => 2010, 'image' => 'images/rain.jpg'],
+    ['title' => 'Property Brothers', 'year' => 2013, 'image' => 'images/property.jpg'],
+    ['title' => 'Race cars', 'year' => 2015, 'image' => 'images/racecar.jpg'],
+    ['title' => 'Dancing', 'year' => 2013, 'image' => 'images/dance.jpg'],
+    ['title' => 'Beach Love', 'year' => 2010, 'image' => 'images/beachlove.jpg'],
+    ['title' => 'Japanese Kitchen', 'year' => 2017, 'image' => 'images/japankitchen.jpg'],
+    ['title' => 'Sport Movie', 'year' => 2015, 'image' => 'images/baseball.jpg'],
+    ['title' => 'Dance Competition', 'year' => 2008, 'image' => 'images/dance2.jpg'],
+    ['title' => 'The Voice', 'year' => 2018, 'image' => 'images/voice.jpg'],
+    ['title' => 'The Voice 2', 'year' => 2012, 'image' => 'images/voice3.jpg'],
+    ['title' => 'Love Stories', 'year' => 2011, 'image' => 'images/love.jpg'],
+    ['title' => 'American Kitchen', 'year' => 2014, 'image' => 'images/kitchen.jpg'],
+    ['title' => 'Modeling, 'year' => 2018, 'image' => 'images/model.jpg']
+];
